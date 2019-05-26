55f0817aa7fc8eeec814a2097287b75301c0f70b
YagoMascarenhas <yago_mascarenhas@live.com>
2019-05-26 19:39:07 -0300

    Lista repositórios do usuário pesquisado

---
+++ main.js
@@ -0,0 +1,51 @@
+var listElement = document.querySelector("#app ul");
+var inputElement = document.querySelector("#app #user");
+var buttonElement = document.querySelector("#app #search");
+
+axios.interceptors.request.use(function(config){
+    listElement.innerHTML = "";
+    var itemElement = document.createElement("li");
+    var itemText = document.createTextNode("Pesquisando...");
+
+    itemElement.appendChild(itemText);
+    listElement.appendChild(itemElement);
+
+    return config;
+});
+
+buttonElement.onclick = function() {
+    var userName = inputElement.value;
+
+    inputElement.value = ""
+
+    axios.get("https://api.github.com/users/" + userName + "/repos?per_page=1000")
+    .then(function(response){
+        listElement.innerHTML = "";
+
+        if(response.data.length === 0){
+            var itemElement = document.createElement("li");
+            var itemText = document.createTextNode("O usuário não tem repositórios");
+
+            itemElement.appendChild(itemText);
+            listElement.appendChild(itemElement);
+        } else {
+            for(var i = 0; i < response.data.length; i++){
+                var itemElement = document.createElement("li");
+                var itemText = document.createTextNode(response.data[i].name);
+                 
+                itemElement.appendChild(itemText);
+                listElement.appendChild(itemElement);
+            }
+        }
+    })
+
+    .catch(function(error){
+        listElement.innerHTML = "";
+
+        var itemElement = document.createElement("li");
+        var itemText = document.createTextNode("Usuário não encontrado.");
+
+        itemElement.appendChild(itemText);
+        listElement.appendChild(itemElement);
+    });
+}
